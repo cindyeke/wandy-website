5800a4defe6b08093213634475a2055a8db9bbf4
cindyeke <officialcee1@gmail.com>
2021-12-01 12:27:09 +0100

    initial commit to dev

--- wandyserver/model/postModel.js
+++ wandyserver/model/postModel.js
@@ -1,165 +1,186 @@
-const sqlConnection = require('../db/db.connection')
+const sqlConnection = require("../db/db.connection");
 
 class Post {
-    constructor(title, description, tag, createdAt, updatedAt, image){
-        this.title = title
-        this.description = description
-        this.tag = tag
-        this.createdAt = createdAt
-        this.updatedAt = updatedAt
-        this.image = image
-    }
+  constructor(title, description, tag, createdAt, updatedAt, image) {
+    this.title = title;
+    this.description = description;
+    this.tag = tag;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.image = image;
+  }
 
-    // Create new post
-    createNewPost(newPost, result){
-        const date = new Date(newPost.createdAt)
+  // Create new post
+  createNewPost(newPost, result) {
+    const date = new Date(newPost.createdAt);
 
-        const values = [newPost.title, newPost.description, newPost.tag, date, date, newPost.image]
-        const sqlStatement = 'INSERT INTO posts (title, description, tag, createdAt, updatedAt, image) VALUES(?)'
+    const values = [
+      newPost.title,
+      newPost.description,
+      newPost.tag,
+      date,
+      date,
+      newPost.image,
+    ];
+    const sqlStatement =
+      "INSERT INTO posts (title, description, tag, createdAt, updatedAt, image) VALUES(?)";
 
-        sqlConnection.query(sqlStatement, [values], (err, res) => {
-            if(err) {
-                console.log('error: ', err)
-                result(err, null)
+    sqlConnection.query(sqlStatement, [values], (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
 
-                return;
-            }
+        return;
+      }
 
-            result(null, 'Success!')
-        })
-    }
+      result(null, "Success!");
+    });
+  }
 
+  // Get all Posts with limits
+  static getAllPostsWithLimits(result, start, end) {
+    const sqlStatement = "SELECT * FROM posts ORDER BY updatedAt DESC LIMIT ?";
+    const queryParams = [parseInt(start), parseInt(end)];
 
-    // Get all Posts with limits
-    static getAllPostsWithLimits(result, start, end) {
-        const sqlStatement = 'SELECT * FROM posts ORDER BY updatedAt DESC LIMIT ?'
-        const queryParams = [parseInt(start), parseInt(end)]
+    sqlConnection.query(sqlStatement, [queryParams], (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
 
-        sqlConnection.query(sqlStatement, [queryParams], (err, res) => {
-            if(err) {
-                console.log('error: ', err)
-                result(null, err)
+        return;
+      }
 
-                return;
-            }
-            
-            result(null, res)
-        })
-    }
+      result(null, res);
+    });
+  }
 
-        // Get all Posts without limits
-        static getAllPosts(result) {
-            const sqlStatement = 'SELECT * FROM posts ORDER BY updatedAt DESC'
-    
-            sqlConnection.query(sqlStatement, (err, res) => {
-                if(err) {
-                    console.log('error: ', err)
-                    result(null, err)
-    
-                    return;
-                }
-                
-                result(null, res)
-            })
-        }
+  // Get all Posts without limits
+  static getAllPosts(result) {
+    const sqlStatement = "SELECT * FROM posts ORDER BY updatedAt DESC";
 
-    static getPostsCount(result) {
-        const sqlStatement = 'SELECT COUNT(*) AS postsCount FROM posts'
+    sqlConnection.query(sqlStatement, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
 
-        sqlConnection.query(sqlStatement, (err, res) => {
-            if(err) {
-                console.log('error: ', err)
-                result(null, err)
+        return;
+      }
 
-                return;
-            }
+      result(null, res);
+    });
+  }
 
-            result(null, res[0].postsCount)
-        })
-    }
+  static getPostsCount(result) {
+    const sqlStatement = "SELECT COUNT(*) AS postsCount FROM posts";
 
-    static getPostsByCategory(result, param) {
-        const val = '%' + param + '%'
-        const sqlStatement = `SELECT * FROM wandy.posts WHERE tag LIKE ? order by updatedAt DESC`
+    sqlConnection.query(sqlStatement, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
 
-        sqlConnection.query(sqlStatement, val, (err, res) => {
-            if(err) {
-                console.log('error: ', err)
-                result(null, err)
+        return;
+      }
 
-                return;
-            }
+      result(null, res[0].postsCount);
+    });
+  }
 
-            result(null, res)
-        })
-    }
+  static getPostsByCategory(result, param) {
+    const val = "%" + param + "%";
+    const sqlStatement = `SELECT * FROM wandy.posts WHERE tag LIKE ? order by updatedAt DESC`;
 
-    updatePost(newPost, id, result) {
-        const crtDate = new Date(newPost.createdAt)
-        const updDate = new Date(newPost.updatedAt)
-        const values = [newPost.title, newPost.description, newPost.tag, crtDate, updDate, newPost.image, parseInt(id)]
-        const sqlStatement = `UPDATE posts SET title = ?, description = ?, tag = ?, createdAt = ?, updatedAt = ?, image = ? WHERE id = ?`
+    sqlConnection.query(sqlStatement, val, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
 
-        sqlConnection.query(sqlStatement, values, (err, res) => {
-            if(err) {
-                console.log('error: ', err)
-                result(null, err)
-                return;
-            }
+        return;
+      }
 
-            result(null, 'Success!')
-        })
-    }
+      result(null, res);
+    });
+  }
 
-    updatePostNoImg(newPost, id, result) {
-        const crtDate = new Date(newPost.createdAt)
-        const updDate = new Date(newPost.updatedAt)
-        const values = [newPost.title, newPost.description, newPost.tag, crtDate, updDate, parseInt(id)]
-        const sqlStatement = `UPDATE posts SET title = ?, description = ?, tag = ?, createdAt = ?, updatedAt = ? WHERE id = ?`
+  updatePost(newPost, id, result) {
+    const crtDate = new Date(newPost.createdAt);
+    const updDate = new Date(newPost.updatedAt);
+    const values = [
+      newPost.title,
+      newPost.description,
+      newPost.tag,
+      crtDate,
+      updDate,
+      newPost.image,
+      parseInt(id),
+    ];
+    const sqlStatement = `UPDATE posts SET title = ?, description = ?, tag = ?, createdAt = ?, updatedAt = ?, image = ? WHERE id = ?`;
 
-        sqlConnection.query(sqlStatement, values, (err, res) => {
-            if(err) {
-                console.log('error: ', err)
-                result(null, err)
-                return;
-            }
+    sqlConnection.query(sqlStatement, values, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
 
-            result(null, 'Success!')
-        })
-    }
+      result(null, "Success!");
+    });
+  }
 
-    // Delete Post
-    static deletePostById(result, id) {
-        console.log('got here 2')
-        const sqlStatement = 'DELETE FROM posts WHERE id = ?'
-        const intID = parseInt(id)
+  updatePostNoImg(newPost, id, result) {
+    const crtDate = new Date(newPost.createdAt);
+    const updDate = new Date(newPost.updatedAt);
+    const values = [
+      newPost.title,
+      newPost.description,
+      newPost.tag,
+      crtDate,
+      updDate,
+      parseInt(id),
+    ];
+    const sqlStatement = `UPDATE posts SET title = ?, description = ?, tag = ?, createdAt = ?, updatedAt = ? WHERE id = ?`;
 
-        sqlConnection.query(sqlStatement, intID, (err, res) => {
-            if(err) {
-                console.log('error: ', err)
-                result(null, err)
+    sqlConnection.query(sqlStatement, values, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
 
-                return;
-            }
-            
-            result(null, 'Success!')
-        })
-    }
+      result(null, "Success!");
+    });
+  }
 
-    static getSocials(result) {
-        const sqlStatement = 'SELECT * FROM social'
+  // Delete Post
+  static deletePostById(result, id) {
+    const sqlStatement = "DELETE FROM posts WHERE id = ?";
+    const intID = parseInt(id);
 
-        sqlConnection.query(sqlStatement, (err, res) => {
-            if(err) {
-                console.log('error: ', err)
-                result(null, err)
+    sqlConnection.query(sqlStatement, intID, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
 
-                return;
-            }
-            
-            result(null, res)
-        })
-    }
+        return;
+      }
+
+      result(null, "Success!");
+    });
+  }
+
+  static getSocials(result) {
+    const sqlStatement = "SELECT * FROM social";
+
+    sqlConnection.query(sqlStatement, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+
+        return;
+      }
+
+      result(null, res);
+    });
+  }
 }
 
-module.exports = Post
+module.exports = Post;
